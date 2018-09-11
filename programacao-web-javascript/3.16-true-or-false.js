d178a0680b1f0bc49acfacc8f3b52a06566b1f3d
Marcelo Cesar <marcelop3@gmail.com>
2018-09-10 22:44:20 -0300

    quiz condicionais

---
+++ programacao-web-javascript/3.16-true-or-false.js
@@ -0,0 +1,46 @@
+if (1) {
+    console.log("o valor é verdadeiro");
+} else {
+    console.log("o valor é falso");
+}
+//o valor é verdadeiro
+
+
+if ("") {
+    console.log("o valor é verdadeiro");
+} else {
+    console.log("o valor é falso");
+}
+//o valor é falso
+
+if ("null") {
+    console.log("o valor é verdadeiro");
+} else {
+    console.log("o valor é falso");
+}
+
+//o valor é verdadeiro
+
+if (-5) {
+    console.log("o valor é verdadeiro");
+} else {
+    console.log("o valor é falso");
+}
+
+//o valor é verdadeiro
+
+if (0.0) {
+    console.log("o valor é verdadeiro");
+} else {
+    console.log("o valor é falso");
+}
+
+//o valor é falso
+
+if (undefined) {
+    console.log("o valor é verdadeiro");
+} else {
+    console.log("o valor é falso");
+}
+
+//o valor é falso
